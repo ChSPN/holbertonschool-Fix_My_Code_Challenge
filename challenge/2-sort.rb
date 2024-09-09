810a93a3bd73d9b40bc5d84da469900734c1e9a6
ChSPN <charlene.scomparin@gmail.com>
2024-09-09 10:36:19 +0200

    2. Sort

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -1,31 +1,12 @@
-###
-#
-#  Sort integer arguments (ascending) 
-#
-###
-
-result = []
-ARGV.each do |arg|
-    # skip if not integer
-    next if arg !~ /^-?[0-9]+$/
-
-    # convert to integer
-    i_arg = arg.to_i
-    
-    # insert result at the right position
-    is_inserted = false
-    i = 0
-    l = result.size
-    while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
-            result.insert(i - 1, i_arg)
-            is_inserted = true
-            break
-        end
+# Check if each argument can be converted to an integer, ignoring invalid ones
+sorted_numbers = ARGV.map do |arg|
+    begin
+      Integer(arg)
+    rescue ArgumentError
+      nil  # Ignore non-integer values
     end
-    result << i_arg if !is_inserted
-end
-
-puts result
+  end.compact.sort
+  
+  # Output the sorted numbers
+  sorted_numbers.each { |num| puts num }
+  
