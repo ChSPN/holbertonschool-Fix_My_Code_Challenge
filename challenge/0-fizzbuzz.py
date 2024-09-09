1bfb638fb1e34763e0af9497289b20bef35e4c78
ChSPN <charlene.scomparin@gmail.com>
2024-09-09 10:24:58 +0200

    try to fix with another way

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -8,31 +8,32 @@ def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
 
-    - For multiples of three print "Fizz" inste    ./0-fizzbuzz.py 89    chmod +x ./0-fizzbuzz.pyd of the number and for
+    - For multiples of three print "Fizz" instead of the number and for
       multiples of five print "Buzz".
     - For numbers which are multiples of both three and five print "FizzBuzz".
     """
-import sys
+    if n < 1:
+        return
 
-def fizzbuzz(n):
+    tmp_result = []
     for i in range(1, n + 1):
-        if i % 15 == 0:  # divisible par 3 et 5
-            print("FizzBuzz", end=" ")
-        elif i % 3 == 0:
-            print("Fizz", end=" ")
-        elif i % 5 == 0:
-            print("Buzz", end=" ")
+        if (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
         else:
-            print(i, end=" ")
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
 
-# Vérification de l'entrée depuis la ligne de commande
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        try:
-            n = int(sys.argv[1])  # Utiliser le premier argument de la ligne de commande comme valeur pour n
-            fizzbuzz(n)
-        except ValueError:
-            print("Veuillez fournir un nombre entier valide.")
-    else:
-        print("Usage: ./0-fizzbuzz.py <nombre>")
 
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
+
+    number = int(sys.argv[1])
+    fizzbuzz(number)
