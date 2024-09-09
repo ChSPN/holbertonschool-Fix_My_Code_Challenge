f409a1964242cdb75b40d47b80d2bd44a8e28a6f
ChSPN <charlene.scomparin@gmail.com>
2024-09-09 10:59:12 +0200

    refix

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,10 +1,11 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Deletes the node at index of a dlistint_t linked list.
- * @head: Double pointer to the head of the list.
- * @index: Index of the node to delete.
- * Return: 1 if successful, -1 if failed.
+ * delete_dnodeint_at_index - Deletes the node at index of a dlistint_t list
+ * @head: Pointer to the pointer of the head of the list
+ * @index: Index of the node that should be deleted (starting from 0)
+ * Return: 1 if successful, -1 if failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
@@ -14,26 +15,20 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
     if (*head == NULL)  /* Empty list case */
         return (-1);
 
-    if (index == 0) {
-        *head = temp->next;  /* Move head to the next node */
-        if (*head != NULL)
-            (*head)->prev = NULL;  /* Remove reference to the old head */
-        free(temp);
-        return (1);
-    }
-
-    for (i = 0; temp != NULL && i < index; i++)
-        temp = temp->next;
-
-    if (temp == NULL)  /* Index is out of range */
-        return (-1);
-
-    if (temp->next != NULL)
-        temp->next->prev = temp->prev;
-
-    if (temp->prev != NULL)
-        temp->prev->next = temp->next;
-
+ if (index == 0) {
+    *head = temp->next;  /* Move head to the next node */
+    if (*head != NULL)
+        (*head)->prev = NULL;  /* Remove reference to the old head */
     free(temp);
     return (1);
 }
+
+if (temp->next != NULL) {
+    temp->next->prev = temp->prev;
+}
+
+if (temp->prev != NULL) {
+    temp->prev->next = temp->next;
+}
+
+free(temp);
