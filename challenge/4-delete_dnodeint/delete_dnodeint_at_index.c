d4e3c1bca9feea48d8599ff25699c273422974d4
ChSPN <charlene.scomparin@gmail.com>
2024-09-09 10:50:17 +0200

    4. Double linked list

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -2,52 +2,47 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Delete a node at a specific index from a list
- *
- * @head: A pointer to the first element of a list
- * @index: The index of the node to delete
- *
- * Return: 1 on success, -1 on failure
+ * delete_dnodeint_at_index - Deletes the node at index of a dlistint_t list
+ * @head: Pointer to the pointer of the head of the list
+ * @index: Index of the node that should be deleted (starting from 0)
+ * Return: 1 if successful, -1 if failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *saved_head;
-	dlistint_t *tmp;
-	unsigned int p;
+    dlistint_t *temp = *head;
+    unsigned int i = 0;
 
-	if (*head == NULL)
-	{
-		return (-1);
-	}
-	saved_head = *head;
-	p = 0;
-	while (p < index && *head != NULL)
-	{
-		*head = (*head)->next;
-		p++;
-	}
-	if (p != index)
-	{
-		*head = saved_head;
-		return (-1);
-	}
-	if (0 == index)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-		if (tmp != NULL)
-		{
-			tmp->prev = NULL;
-		}
-	}
-	else
-	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
-		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
-		*head = saved_head;
-	}
-	return (1);
+    if (*head == NULL)  /* Empty list case */
+        return (-1);
+
+    /* If we're deleting the head node */
+    if (index == 0)
+    {
+        *head = temp->next;  /* Move head to the next node */
+        if (*head != NULL)
+            (*head)->prev = NULL;  /* Remove reference to the old head */
+        free(temp);  /* Free the old head */
+        return (1);
+    }
+
+    /* Traverse the list to find the node at the given index */
+    while (temp != NULL && i < index)
+    {
+        temp = temp->next;
+        i++;
+    }
+
+    /* If we reach the end without finding the node */
+    if (temp == NULL)
+        return (-1);
+
+    /* Adjust pointers to bypass the node at index */
+    if (temp->next != NULL)  /* Not the last node */
+        temp->next->prev = temp->prev;
+
+    if (temp->prev != NULL)  /* Not the first node */
+        temp->prev->next = temp->next;
+
+    free(temp);  /* Free the node */
+    return (1);
 }
